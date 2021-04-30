72acac2ae1dc2900614b1e5699871b3815c79180
cosmetico2013 <serlagee44@gmail.com>
2021-04-30 19:47:09 +0200

    video 8

--- proyecto/boletin/admin.py
+++ proyecto/boletin/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Registrado		
+class AdminRegistrado(admin.ModelAdmin):
+	list_display = ["email", "nombre", "timestamp"]
+	#list_display_links = ["nombre"] #campo que tienen el link hacia la cuenta del usuario
+	list_filter = ["timestamp"]
+	list_editable = ["nombre"] #campo que se permite modificar
+	search_fiels = ["email", "nombre"] #campos que se pueden buscar
+	class Meta:
+		model = Registrado
 
-admin.site.register(Registrado)
+admin.site.register(Registrado, AdminRegistrado)
