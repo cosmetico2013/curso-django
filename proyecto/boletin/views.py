97d51672461bbd9a434fb4a14c6d297c7ea7a854
cosmetico2013 <serlagee44@gmail.com>
2021-05-02 16:22:50 +0200

    video 14

--- proyecto/boletin/views.py
+++ proyecto/boletin/views.py
@@ -4,7 +4,9 @@ from .forms import RegForm
 # Create your views here.
 
 def inicio(request):
-	form = RegForm()
+	form = RegForm(request.POST or None)
+	if form.is_valid():
+		form_data = form.cleaned_data
 	context = {
 		"el_form" : form,
 	}
