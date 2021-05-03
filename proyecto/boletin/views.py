af68ece7dcee05d15391c66bb7f7cab5001c05dd
cosmetico2013 <serlagee44@gmail.com>
2021-05-03 03:56:36 +0200

    video 18

--- proyecto/boletin/views.py
+++ proyecto/boletin/views.py
@@ -5,13 +5,17 @@ from .models import Registrado
 # Create your views here.
 
 def inicio(request):
-	form = RegForm(request.POST or None)
+	titulo = "HOLA"
+	if  request.user.is_authenticated:
+		titulo = "Bienvenido %s" %(request.user)
+	f orm = RegForm(request.POST or None)
 	if form.is_valid():
 		form_data = form.cleaned_data
 		abc = form_data.get("email")
 		abc2 = form_data.get("nombre")
 		obj = Registrado.objects.create(email=abc, nombre=abc2)
 	context = {
+		"titulo" : titulo,
 		"el_form" : form,
 	}
 	return render (request, "inicio.html", context)
