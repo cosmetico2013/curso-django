672dd876dfcfc900eb0498125356c58c1bc5df44
cosmetico2013 <serlagee44@gmail.com>
2021-05-03 04:25:39 +0200

    videos 19

--- proyecto/boletin/views.py
+++ proyecto/boletin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import RegForm
+from .forms import RegForm, RegModelForm
 from .models import Registrado
 
 # Create your views here.
@@ -8,14 +8,30 @@ def inicio(request):
 	titulo = "HOLA"
 	if  request.user.is_authenticated:
 		titulo = "Bienvenido %s" %(request.user)
-	f orm = RegForm(request.POST or None)
-	if form.is_valid():
-		form_data = form.cleaned_data
-		abc = form_data.get("email")
-		abc2 = form_data.get("nombre")
-		obj = Registrado.objects.create(email=abc, nombre=abc2)
+	form = RegModelForm(request.POST or None)
+	
 	context = {
 		"titulo" : titulo,
 		"el_form" : form,
 	}
+
+	if form.is_valid():
+		instance = form.save(commic=False)
+		if not instance.nombre:
+			instance.nombre = "Persona" 
+		instance.save()
+
+		context = {
+			"titulo" : "Gracias %s" %(instance.nombre)
+		}
+
+		if not nombre:
+			context = {
+				"titulo": "gracias %s" %(instance.email)
+			}
+#		form_data = form.cleaned_data
+#		abc = form_data.get("email")
+#		abc2 = form_data.get("nombre")
+#		obj = Registrado.objects.create(email=abc, nombre=abc2)
+	
 	return render (request, "inicio.html", context)
