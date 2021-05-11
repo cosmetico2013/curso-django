6ff0fcbc01408861ec75a7cf472e33f82cb7a6a6
cosmetico2013 <serlagee44@gmail.com>
2021-05-11 02:06:28 +0200

    video 29

--- proyecto/boletin/views.py
+++ proyecto/boletin/views.py
@@ -39,6 +39,7 @@ def inicio(request):
 	return render (request, "inicio.html", context)
 
 def contact(request):
+	titulo = "Contacto"
 	form = ContactForm(request.POST or None)
 	if form.is_valid():
 		#for key, value in form.cleaned_data.iteritems():
@@ -63,5 +64,6 @@ def contact(request):
 		#print email, mensaje nombre
 	context = {
 		"form": form,
+		"titulo": titulo,
 	}
 	return render(request, "forms.html", context)
