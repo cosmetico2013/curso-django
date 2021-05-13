fa6db10aebfc210e4dec6284ab250cfa2d62442f
cosmetico2013 <serlagee44@gmail.com>
2021-05-13 13:48:31 +0200

    video 39

--- proyecto/boletin/views.py
+++ proyecto/boletin/views.py
@@ -33,8 +33,9 @@ def inicio(request):
 			}
 	
 	if request.user.is_authenticated and request.user.is_staff:
+		queryset = Registrado.objects.all().order_by("-timestamp")
 		context = {
-			"queryset": ["abc", "123"],
+			"queryset": queryset,
 		}
 	return render (request, "inicio.html", context)
 
